057d5afa673cb699146ae40ab446d41083a6bae5
t100ta <cjx.open_d@me.com>
2018-07-08 01:45:15 +0900

    Roll-a-Ball Complete

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+
+	void Update () {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+
+        if(count == 0)
+        {
+            winnerLabelObject.SetActive(true);
+        }
+	}
+}
