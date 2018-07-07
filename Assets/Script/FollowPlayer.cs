3b060c127b44ee781877074d789e2cc64fd7c5e5
t100ta <cjx.open_d@me.com>
2018-07-07 19:19:14 +0900

    first commit

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+
+    void Update () {
+
+        GetComponent<Transform>().position = target.position + offset;
+
+	}
+}
---
+++ Assets/Script/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider hit)
+    {
+
+        if (hit.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    /* https://unity3d.com/jp/learn/tutorials/projects/hajiuni/creating-the-game-ui?playlist=45986 */
+}
