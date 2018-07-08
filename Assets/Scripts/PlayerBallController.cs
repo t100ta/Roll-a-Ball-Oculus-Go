b73212a8f9e231c7722fcbb954a836fdb387f0a4
t100ta <cjx.open_d@me.com>
2018-07-08 17:10:46 +0900

    add the oculus go controller

---
+++ Assets/Scripts/PlayerBallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBallController : MonoBehaviour
+{
+
+    public float speed = 10;
+
+    void FixedUpdate()
+    {
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+        
+        Vector2 vector = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
+        float x = vector.x;
+        float z = vector.y;
+
+#else
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+    #endif
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(x * speed, 0, z * speed);
+
+    }
+}
